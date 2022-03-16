92db036f63a9e4cc126ea16ddfc038a5119258a8
Laureano2279 <79203128+Laureano2279@users.noreply.github.com>
2022-03-16 18:28:58 -0300

    pre entrega

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-const agregarCarrito = (producto) => {
-    carrito.push(producto)
+const compraResumen = (nombre, precio) => {
+    agregarCarrito (nombre, precio)
+    console.log(`Se agrego al carrito ${nombre} $ ${precio}`);
+    total += precio;  
+    console.log(total)
+}
+let compra;
+let total = 0;
+
+
+const agregarCarrito = (nombre, precio) => {
+    carrito.push(new Producto(nombre, precio))
 }
 const carrito = [];
 const productos = [
@@ -12,16 +22,59 @@ const productos = [
     { id: 7, titulo: "Choclos", precio: 250 },
     { id: 8, titulo: "Tomates", precio: 245 },
 ]
-
-agregarCarrito(productos [0]);
-console.log(carrito);
-
-productos.forEach(elementoVerduras =>{
-    console.log(elementoVerduras)
-})
-
-if (document.readyState == 'loading') {
-    document.addEventListener('DOMContentLoaded', ready)
-} else {
-    ready()
+class Producto {
+    constructor(nombre, precio) {
+        this.nombre = nombre;
+        this.precio = parseInt(precio);
+    }
 }
+
+const usuario = prompt('Ingrese nombre: ')
+do {
+    const productosComprar = parseInt (prompt(`Ingrese el numero del producto que desea comprar:
+    1 - ${productos[0].titulo} $ ${productos[0].precio}
+    2 - ${productos[1].titulo} $ ${productos[1].precio}
+    3 - ${productos[2].titulo} $ ${productos[2].precio}
+    4 - ${productos[3].titulo} $ ${productos[3].precio}
+    5 - ${productos[4].titulo} $ ${productos[4].precio}
+    6 - ${productos[5].titulo} $ ${productos[5].precio}
+    7 - ${productos[6].titulo} $ ${productos[6].precio}
+    8 - ${productos[7].titulo} $ ${productos[7].precio}`));
+
+    switch (productosComprar){
+        case 1:
+            compraResumen (productos[0].titulo, productos[0].precio) 
+            break;
+        case 2:
+            compraResumen (productos[1].titulo, productos[1].precio) 
+            break;
+        case 3:
+            compraResumen (productos[2].titulo, productos[2].precio) 
+            break;
+        case 4:
+            compraResumen (productos[3].titulo, productos[3].precio) 
+            break;
+        case 5:
+            compraResumen (productos[4].titulo, productos[4].precio) 
+            break;
+        case 6:
+            compraResumen (productos[5].titulo, productos[5].precio) 
+            break;
+        case 7:
+            compraResumen (productos[6].titulo, productos[6].precio) 
+            break;
+        case 8:
+            compraResumen (productos[7].titulo, productos[7].precio) 
+            break;
+        
+        default: alert('Numero ingresado es incorrecto');
+        break;
+    }
+
+     compra = prompt('Deseas seguir comprando? S/N')
+
+}
+    while (compra !== "n");
+console.log(carrito) 
+
+console.log(`El total de tu compra es: $ ${total}`)
